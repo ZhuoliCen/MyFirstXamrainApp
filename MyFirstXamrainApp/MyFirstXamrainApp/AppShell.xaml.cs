2120373b6cc2ebad0682e6d2d0f2c1d576e189c7
15563 <15563@DESKTOP-0149F5U>
2022-02-01 22:21:43 +1100

    Add project files.

---
+++ MyFirstXamrainApp/MyFirstXamrainApp/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using MyFirstXamrainApp.ViewModels;
+using MyFirstXamrainApp.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace MyFirstXamrainApp
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
---
+++ MyFirstXamrainApp/MyFirstXamrainApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MyFirstXamrainApp.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace MyFirstXamrainApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
